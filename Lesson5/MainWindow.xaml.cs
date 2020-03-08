259b2071133a8731cdcbeab3ebb586d79860bf32
Владислав <Death_Shadow@list.ru>
2020-03-08 16:36:35 +0400

    Добавьте файлы проекта.

---
+++ Lesson5/MainWindow.xaml.cs
@@ -0,0 +1,101 @@
+﻿using Lesson5.Classes;
+using Lesson5.Windows;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Lesson5
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        internal static DB db;
+        public MainWindow()
+        {
+            InitializeComponent();
+            db = new DB();
+            empList.ItemsSource = db.GetEmployees();
+            cbDepList.ItemsSource = db.GetDeptaments();
+            db.updateData += Update;
+        }
+
+        /// <summary>Обработка события выбора элемента из списка</summary>
+        /// <param name="sender">Объект</param>
+        /// <param name="args">Параметры</param>
+        private void Selected(object sender, SelectionChangedEventArgs args)
+        {
+            tbInfo.Text = db.GetInfo(sender);
+        }
+
+        /// <summary>Обработка нажатия кнопки "редактировать департамент"</summary>
+        /// <param name="sender">Объект</param>
+        /// <param name="args">Параметры</param>
+        private void BtnEditDep_Click(object sender, RoutedEventArgs e)
+        {
+            if (cbDepList.SelectedItem != null)
+            {
+                DepEditWindow depEditWindow = new DepEditWindow(cbDepList.SelectedItem.ToString());
+                depEditWindow.Owner = this;
+                depEditWindow.Show();
+            }
+            else
+                MessageBox.Show("Выберете отдел для редактирования!");
+        }
+
+        /// <summary>Обработка нажатия кнопки "редактировать сотрудника"</summary>
+        /// <param name="sender">Объект</param>
+        /// <param name="args">Параметры</param>
+        private void BtnEditEmp_Click(object sender, RoutedEventArgs e)
+        {
+            if (empList.SelectedItem != null)
+            {
+                EmpEditWindow empEditWindow = new EmpEditWindow(empList.SelectedItem as Employee);
+                empEditWindow.Owner = this;
+                empEditWindow.Show();
+            }
+            else
+                MessageBox.Show("Выберете сотрудника для редактирования!");
+        }
+
+        /// <summary>Обработка нажатия кнопки "добавить сотрудника"</summary>
+        /// <param name="sender">Объект</param>
+        /// <param name="args">Параметры</param>
+        private void BtnCreateEmp_Click(object sender, RoutedEventArgs e)
+        {
+            AddEmpWindow addEmpWindow = new AddEmpWindow();
+            addEmpWindow.Owner = this;
+            addEmpWindow.Show();
+        }
+
+        /// <summary>Обработка нажатия кнопки "добавить департамент"</summary>
+        /// <param name="sender">Объект</param>
+        /// <param name="args">Параметры</param>
+        private void BtnCreateDep_Click(object sender, RoutedEventArgs e)
+        {
+            AddDepWindow addDepWindow = new AddDepWindow();
+            addDepWindow.Owner = this;
+            addDepWindow.Show();
+        }
+
+        /// <summary>Обновляет данные на форме</summary>
+        internal void Update()
+        {
+            empList.ItemsSource = db.GetEmployees();
+            cbDepList.ItemsSource = db.GetDeptaments();
+        }
+    }
+}
---
+++ Lesson5/Windows/AddDepWindow.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using Lesson5.Classes;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Lesson5.Windows
+{
+    /// <summary>
+    /// Логика взаимодействия для AddDepWindow.xaml
+    /// </summary>
+    public partial class AddDepWindow : Window
+    {
+        public AddDepWindow()
+        {
+            InitializeComponent();
+        }
+        /// <summary>Обработчик нажатия кнопки "сохранить"</summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void BtnSaveDep_Click(object sender, RoutedEventArgs e)
+        {
+            if (MainWindow.db.addDep(tboxNewDep.Text))
+            {
+                MessageBox.Show("Отдел добавлен!");
+                this.Close();
+            }
+            else
+                MessageBox.Show("Такой отдел уже существует!");
+        }
+    }
+}
